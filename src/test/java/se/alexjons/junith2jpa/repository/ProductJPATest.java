66ae33b5340c6027a40f80a50fac925c48e50ce4
alexjons987 <alexander.jonsson987@outlook.com>
2025-12-02 16:09:13 +0100

    Add custom query test

--- src/test/java/se/alexjons/junith2jpa/repository/ProductJPATest.java
+++ src/test/java/se/alexjons/junith2jpa/repository/ProductJPATest.java
@@ -7,8 +7,10 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 import se.alexjons.junith2jpa.entity.Product;
 
+import java.util.List;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ActiveProfiles("test")
@@ -38,5 +40,30 @@ class ProductJPATest {
         assertEquals(savedBanana.getId(), product.get().getId());
         assertEquals(savedBanana.getName(), product.get().getName());
         assertEquals(savedBanana.getPrice(), product.get().getPrice());
+    }
+
+    @Test
+    @DisplayName("findByName should return previously saved product")
+    void findByName() {
+        // Arrange
+        Product banana = new Product("Banan", 9.99d);
+        Product apple = new Product("Äpple", 10.99d);
+        Product blueberries = new Product("Blåbär", 11.99d);
+
+        Product savedBanana = productRepository.save(banana);
+        productRepository.save(apple);
+        productRepository.save(blueberries);
+
+        // Act
+        List<Product> result = productRepository.findByName(savedBanana.getName());
+
+        // Assert
+        assertThat(result).hasSize(1);
+        assertThat(result)
+                .usingRecursiveFieldByFieldElementComparator()
+                .containsExactly(savedBanana);
+        assertThat(result)
+                .extracting(Product::getName)
+                .doesNotContain("Äpple", "Blåbär");
     }
 }
