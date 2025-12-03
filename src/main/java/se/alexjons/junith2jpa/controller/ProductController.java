16b46903dff38a7803c4ee0d0bf78617bacb2567
alexjons987 <alexander.jonsson987@outlook.com>
2025-12-03 14:19:33 +0100

    Add controller test

--- src/main/java/se/alexjons/junith2jpa/controller/ProductController.java
+++ src/main/java/se/alexjons/junith2jpa/controller/ProductController.java
@@ -1,11 +1,24 @@
 package se.alexjons.junith2jpa.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import se.alexjons.junith2jpa.dto.ProductResponseDTO;
+import se.alexjons.junith2jpa.service.ProductService;
 
 @Validated
 @RestController
-@RequestMapping("/products")
+@RequestMapping("/api/products")
 public class ProductController {
+
+    @Autowired
+    ProductService productService;
+
+    @GetMapping("/{id}")
+    public ProductResponseDTO getProduct(@PathVariable Long id) {
+        return productService.getProductById(id).get();
+    }
 }
---
+++ src/test/java/se/alexjons/junith2jpa/controller/ProductControllerIntegrationTest.java
@@ -0,0 +1,47 @@
+package se.alexjons.junith2jpa.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import se.alexjons.junith2jpa.entity.Product;
+import se.alexjons.junith2jpa.repository.ProductRepository;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ActiveProfiles("test")
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
+class ProductControllerIntegrationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ProductRepository productRepository;
+
+    private Product savedProduct;
+
+    @BeforeEach
+    void setUp() {
+        productRepository.deleteAll();
+
+        Product banana = new Product("Banana", 9.99d);
+        savedProduct = productRepository.save(banana);
+    }
+
+    @Test
+    @DisplayName("")
+    void getProductByIdReturnProduct() throws Exception {
+        mockMvc.perform(get("/api/products/" + savedProduct.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Banana"))
+                .andExpect(jsonPath("$.price").value(9.99d));
+    }
+}
